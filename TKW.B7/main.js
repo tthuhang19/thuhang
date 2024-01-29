f0ef45e4cca1816b8f06384e25eef69fb8c0577e
tthuhang19 <152346655+tthuhang19@users.noreply.github.com>
2024-01-29 23:50:26 +0700

    Add files via upload

---
+++ TKW.B7/main.js
@@ -0,0 +1,8 @@
+function myclick(){
+   
+    document.getElementById('list__child-active').classList.toggle('active-list__child')
+}
+function myclick1(){
+   
+    document.getElementById('list__child-active-1').classList.toggle('active-list__child')
+}
